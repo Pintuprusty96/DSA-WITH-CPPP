5e355fab9f0470c38331e406c8690f01cd3e2c87
Pintuprusty16@gmail.com <pintuprusty16@gmail.com>
2023-10-06 23:49:05 +0530

    day12

--- pattern1.cpp
+++ pattern1.cpp
@@ -1,18 +1,20 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    int i,j,k,l;
+int main()
+{
+    int i, j, k, l;
 
-    for(i=1; i<=5; i++){
-
-        for(j=1; i=j<= i-1; j++){
-            cout<<'-';
+    for (i = 1; i <= 5; i++)
+    {
+        for (j = 1; i = j <= i - 1; j++)
+        {
+            cout << '-';
         }
-
-        for(k=1; k<=i; k++){
-            cout<<k;
+        for (k = 1; k <= i; k++)
+        {
+            cout << k;
         }
-        cout<<endl;
+        cout << endl;
     }
 }
