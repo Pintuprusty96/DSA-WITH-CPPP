540943c954efb2f5be4292607fe70f131c8819c9
Pintu Prusty <pintuprusty16@gmail.com>
2023-10-08 23:37:54 +0530

    day15

---
+++ day15/tempCodeRunnerFile.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+   /*
+   1: Take a number n from the user 
+   and print all the even numbers between 1 
+   and n(inclusive). Do this using while and 
+   do while loop separately.
