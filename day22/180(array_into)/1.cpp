6046270f774bc37f8378bde25253f4f3a18374b0
Pintuprusty16@gmail.com <pintuprusty16@gmail.com>
2023-10-10 01:14:37 +0530

    day22

---
+++ day22/180(array_into)/1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    /*1: Take 20 elements from user input and 
+    find its sum with the help of an array.
+    int arr [6];
+    int sum=0;
+
+    for(int i=0; i<6; i++){
+        cin>>arr[i];
+    }
+    for(int i=0; i<6; i++){
+        sum=sum+arr[i];
+    }
+    cout<<sum<<" ";
+     */
+
+    /*
+        4: Create an array of char types and store ‘a’ to ‘z’ in it. Then print the element of the arrays.
+    */
+   int arr [5]= {'a','b','c','h','j'};
+
+   cout<<sizeof(arr);
+   for(int i=0; i<5; i++){
+    cout<<arr[i];
+   }
+}
