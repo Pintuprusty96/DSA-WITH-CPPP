c8707ff4c40b1054e331bd63628e348c5bce41e5
Pintu Prusty <pintuprusty16@gmail.com>
2023-10-10 23:35:33 +0530

    day22/180(array_intro)

---
+++ day22/180(array_into)/1.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    /*1: Take 20 elements from user input and
+    find its sum with the help of an array.
+    int arr [6];
+    int sum=0;
+
+    for(int i=0; i<6; i++){
+        cin>>arr[i];
+    }
+    for(int i=0; i<6; i++){
+        sum=sum+arr[i];
+    }
+    cout<<sum<<" ";
+     */
+
+    /*
+        Day 22/180 Introduction to Arrays in c++
+
+
+    \/1: Take 20 elements from user input and find its sum with the help of an array.
+
+4: Create an array of char types and store ‘a’ to ‘z’ in it. Then print the element of the arrays.
+
+5: Find the second largest element in an array of unique elements of size n. Where n>3.
+
+6: Find the third smallest element in an array of unique elements size n. Where n>3.
+
+7: What is Byte addressable?
+
+
+        4: Create an array of char types and store ‘a’ to ‘z’ in it. Then print the element of the arrays.
+    */
+    /*
+    2: Calculate the average of elements in an array of size 18.
+        int size[]={1,2,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18};
+        cout<<sizeof(size)/sizeof(size[0]);
+    */
+    /*
+ 3: Find the index of a specific element in an array, 
+ if the element is nor present, print -1. Ask the size of the array from the user and then implement it.
+   int arr[]={1,2.3,4,5,6,7,8,9,10};
+    cout<<sizeof(arr)/sizeof(arr[0]);
+//    cin>>ele;
+    int ele=22;
+    int bag;
+   for(int i=0; i<11; i++){
+        if(arr[i]==ele){
+            bag=arr[i];
+        }
+   }
+   if(bag==ele){
+    cout<<bag;
+   }
+   else{
+    cout<<"no";
+   }
+   return 0;
+    */
+   /*
+4: Create an array of char types and store ‘a’ to ‘z’ in it. Then print the element of the arrays.
+  char chr[]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+  for(int i=0; i<26; i++){
+    cout<<chr[i]<<' ';
+  }
+   */
+  
+}
