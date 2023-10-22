cc95aeb3a76407d29a4b0959cd70f2f0cee11c44
Pintuprusty16@gmail.com <pintuprusty16@gmail.com>
2023-10-22 23:38:57 +0530

    day31(binary_problem

--- day31(binary_problem)/1.cpp
+++ day31(binary_problem)/1.cpp
@@ -104,7 +104,6 @@ The returned integer should be non-negative as well.
  You must not use any built-in exponent function or operator.
 
  For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
-    */
    int x;
    cin>>x;
    int start=0; 
@@ -125,4 +124,5 @@ The returned integer should be non-negative as well.
    }
    cout<<ans;
 
+    */
 }
